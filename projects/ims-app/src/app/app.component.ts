f120de427c9ae7c7833ed62df87a8435936e7b9f
Kobi Hari <kobi2294@yahoo.com>
2026-06-28 10:41:46 +0300

    feat: integrate ims-infra library and update TypeScript configurations

--- projects/ims-app/src/app/app.component.ts
+++ projects/ims-app/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ImsInfra } from '@ims';
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ImsInfra],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
