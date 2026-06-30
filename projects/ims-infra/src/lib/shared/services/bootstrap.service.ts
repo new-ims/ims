7f5466cbc90bc4ab75cb20aa1a4aa14a989401db
Kobi Hari <kobi2294@yahoo.com>
2026-06-30 16:09:55 +0300

    feat: update BootstrapService and ConfigStore for improved state management and initialization handling

--- projects/ims-infra/src/lib/shared/services/bootstrap.service.ts
+++ projects/ims-infra/src/lib/shared/services/bootstrap.service.ts
@@ -1,39 +1,43 @@
-import { inject, Service } from "@angular/core";
-import { ApiService } from "./api.service";
-import { REGISTRY_TOKEN } from "./process-config";
-import { ConfigStore } from "../stores/config/config.store";
+import { inject, Service } from '@angular/core';
+import { ApiService } from './api.service';
+import { REGISTRY_TOKEN } from './process-config';
+import { ConfigStore } from '../stores/config/config.store';
 
 @Service()
 export class BootstrapService {
-    readonly #api = inject(ApiService);
-    readonly #registry = inject(REGISTRY_TOKEN);
-    readonly #configStore = inject(ConfigStore);
-    
-    #delay(millis: number) {
-        return new Promise(resolve => setTimeout(resolve, millis));
+  readonly #api = inject(ApiService);
+  readonly #registry = inject(REGISTRY_TOKEN);
+  readonly #configStore = inject(ConfigStore);
+
+  #delay(millis: number) {
+    return new Promise((resolve) => setTimeout(resolve, millis));
+  }
+
+  async start() {
+    this.getConfigInformation();
+  }
+
+  async getConfigInformation() {
+    console.log('BootstrapService started');
+    // read sessionManagerId query param from url
+    const url = new URL(window.location.href);
+    const sessionManagerId = url.searchParams.get('sessionManagerId');
+    if (!sessionManagerId) {
+      console.error('sessionManagerId query param is missing');
+      return;
     }
 
-    async start() {
-        console.log('BootstrapService started');
-        // read sessionManagerId query param from url
-        const url = new URL(window.location.href);
-        const sessionManagerId = url.searchParams.get('sessionManagerId');
-        if (!sessionManagerId) {
-            console.error('sessionManagerId query param is missing');
-            return;
-        }
+    const input = { sessionManagerId };
+    await this.#delay(2000); // wait for 1 second to simulate some delay
+    const output = await this.#api.externalLogin(input);
+    console.log('External login output:', output);
+    await this.#delay(2000); // wait for 1 second to simulate some delay
 
-        const input = { sessionManagerId };
-        await this.#delay(2000); // wait for 1 second to simulate some delay
-        const output = await this.#api.externalLogin(input);
-        console.log('External login output:', output);
-        await this.#delay(2000); // wait for 1 second to simulate some delay
-
-        const processType = output.processType;
-        const configurer = await this.#registry(processType);
-        const config = configurer();
-        console.log('config: ', config);
-        this.#configStore.setConfig(config);
-        console.log('BootstrapService finished');
-    }
+    const processType = output.processType;
+    const configurer = await this.#registry(processType);
+    const config = configurer();
+    console.log('config: ', config);
+    this.#configStore.setConfig(config);
+    console.log('BootstrapService finished');
+  }
 }
--- projects/ims-infra/src/lib/shared/stores/config/config.store.ts
+++ projects/ims-infra/src/lib/shared/stores/config/config.store.ts
@@ -1,14 +1,17 @@
-import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
-import { initialConfigSlice } from "./config.slice";
-import { ProcessConfig } from "@ims";
+import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
+import { initialConfigSlice } from './config.slice';
+import { ProcessConfig } from '@ims';
+import { computed } from '@angular/core';
 
 export const ConfigStore = signalStore(
-    { providedIn: 'root' },
-    withState(initialConfigSlice), 
-    withMethods(store => ({
-        setConfig(config: ProcessConfig) {
-            patchState(store, { config });
-        }
-    }))
+  { providedIn: 'root' },
+  withState(initialConfigSlice),
+  withComputed((store) => ({
+    isInitialized: computed(() => store.config() !== null),
+  })),
+  withMethods((store) => ({
+    setConfig(config: ProcessConfig) {
+      patchState(store, { config });
+    },
+  })),
 );
-
