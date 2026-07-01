ee489bb9e50c9d56bc68ffca9ad3303236915484
jennyEventov <50540032+jennyEventov@users.noreply.github.com>
2026-07-01 10:17:40 +0300

    Update base-operative-claim.ts

--- common/model/base-operative-claim.ts
+++ common/model/base-operative-claim.ts
@@ -1,2 +1,17 @@
 export  interface BaseOperativeClaim {
+  "@class": string;
+    company: Company;
+    claimNum: number;
+    claimType: CodeValue;
+    claimStatus: CodeValue;
+    overdueClaimCode: number;
+    insuredId: number;
+    policies: IClaimPolicy[];
+    showCancelFlag: boolean;
+    claimGroupNum: number;
+    paymentTemplates: IPaymentTemplate[];
+    irregularClaimMarks: IIrregularClaimMark[];
+    claimLines: IClaimLine[];
+    actionCode: string;
+    insuredProfessionsAtEventDate: CodeValue[]
 }
