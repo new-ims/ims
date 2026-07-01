f3e2bb982e53e7a424af977738a141de667a5b5a
jennyEventov <e.jenny.y@gmail.com>
2026-07-01 10:16:15 +0300

    feat: update NursingClaimProcess to use BaseSuperClaim instead of AmbulatorySuperClaim

--- common/model/nursing-claim-process.ts
+++ common/model/nursing-claim-process.ts
@@ -1,8 +1,9 @@
 import type { ClaimProcess } from './claim-base-process';
+import type { BaseSuperClaim } from './base-super-claim';
 
 export interface NursingClaimProcess extends ClaimProcess {
   readonly _id: string;
   readonly className: 'il.co.migdal.ins.nursing.model.NursingClaimProcess';
   readonly processType: 'NURSING_CLAIM';
-//   readonly superClaim: AmbulatorySuperClaim;
+  readonly superClaim: BaseSuperClaim;
 }
