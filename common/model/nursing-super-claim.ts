89920c1314b256ede33e0bf2b8a8318efa7dc894
jennyEventov <50540032+jennyEventov@users.noreply.github.com>
2026-07-01 08:45:51 +0300

    Update nursing-super-claim.ts

---
+++ common/model/nursing-super-claim.ts
@@ -0,0 +1,14 @@
+export interface NursingSuperClaim extends BaseSuperClaim {
+  "@class": string;
+  operativeClaims: INursingOperativeClaim[];
+  otherClaimResult: number;
+  otherClaim: number;
+newSuperClaimType: number;
+  needToCalculatePeriod: number;
+  physicianDecisionHistList: ILifePhysicianDecision[];
+  loadHistory: boolean;
+  previousOperativeClaims: ILifeDisplayOperativeClaim[];
+  physicianDecisions: ILifePhysicianDecision[];
+  operativeClaims: ILifeOperativeClaim[];
+  infoMessage: string;
+}
